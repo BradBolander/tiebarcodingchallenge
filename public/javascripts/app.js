259470affd66fc32d7624093539c401e211b208e
BradBolander <bolanderbrad@gmail.com>
2016-03-25 21:51:46 -0500

    added product list view and 3D render of a bowtie in Three.js

---
+++ public/javascripts/app.js
@@ -0,0 +1,85 @@
+$('#render-toggle').click(function(){
+  $('#tie-render').toggle(500);
+  $('#hint').toggle(500);
+});
+$('#container').addClass('magictime puffIn');
+
+$('#render-toggle').show();
+$('#render-toggle').addClass('magictime puffIn');
+
+
+var TiesContainer = React.createClass({
+  componentDidMount: function() {
+    this.loadData();
+  },
+  loadData: function() {
+    $.ajax({
+      type: 'GET',
+      url: "/javascripts/ties.json",
+      dataType: 'json',
+      cache: 'false',
+      async: false,
+      complete: function(obj) {
+        console.log(obj.responseJSON.ties);
+        this.setState({data: obj.responseJSON.ties});
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  render: function() {
+    return (
+      <div id="app">
+      <TieRender/>
+      <p className="block-title">NECKTIES</p>
+      <TiesGrid data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var TiesGrid = React.createClass({
+  render: function() {
+    var tieGrid = this.props.data.map(function(tie, index) {
+      console.log(tie);
+      return (
+        <Tie title={tie.title} color={tie.color} url={tie.url} key={tie.id} />
+      );
+    });
+    return (
+      <div id="tiesContainer" >
+          { tieGrid }
+      </div>
+    );
+  }
+});
+
+var Tie = React.createClass({
+  render: function() {
+    return (
+      <div className="tie-block">
+        <img className="tie-image" src={this.props.url} ></img>
+        <div className="title-backdrop">
+          <p className="tie-color muted">{this.props.color}</p>
+          <h4 className="tie-title">{this.props.title}</h4>
+        </div>
+      </div>
+    )
+  }
+});
+
+var TieRender = React.createClass({
+  render: function() {
+    return (
+      <div className="tie-render">
+
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <TiesContainer/>,
+  document.getElementById('container')
+);
---
+++ public/javascripts/tierender.js
@@ -0,0 +1,93 @@
+var container, stats;
+
+			var camera, cameraTarget, scene, renderer;
+
+			var mouseX = 0, mouseY = 0;
+
+			var mouseState = 0;
+
+			var windowHalfX = window.innerWidth / 2;
+			var windowHalfY = window.innerHeight / 2;
+
+			init();
+			animate();
+
+			function init() {
+
+
+				container = document.getElementById( "tie-render" );
+        document.body.appendChild( container );
+				camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 15 );
+				camera.position.set( -.5, .15, 2);
+
+				cameraTarget = new THREE.Vector3( 1, -0.25, -1 );
+
+				scene = new THREE.Scene();
+
+        var manager = new THREE.LoadingManager();
+        var textureLoader = new THREE.ImageLoader(manager);
+        var texture = new THREE.Texture();
+
+				textureLoader.load( '/textures/polka.jpg', function ( image ) {
+					texture.image = image;
+					texture.needsUpdate = true;
+				} );
+
+        var modelLoader = new THREE.OBJLoader();
+				modelLoader.load( "/models/BowTie2.obj", function ( object ) {
+          object.traverse(function(child) {
+						if ( child instanceof THREE.Mesh ) {
+							child.material.map = texture;
+						}
+
+          });
+					scene.add( object );
+				});
+				// Lights
+
+        var light = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1 );
+        scene.add( light );
+
+				// renderer
+				renderer = new THREE.WebGLRenderer( { antialias: true, alpha:true } );
+
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.shadowMap.enabled = true;
+				renderer.shadowMap.cullFace = THREE.CullFaceBack;
+				container.appendChild( renderer.domElement );
+				window.addEventListener( 'resize', onWindowResize, false );
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+			}
+
+			function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+			}
+
+			function animate() {
+				requestAnimationFrame( animate );
+				render();
+			}
+
+			function onDocumentMouseMove( event ) {
+				mouseX = ( event.clientX - windowHalfX ) / 2;
+				mouseY = ( event.clientY - windowHalfY ) / 2;
+			}
+
+			function render() {
+				document.body.onmousedown = function() {
+					mouseState = 1;
+				}
+				document.body.onmouseup = function() {
+					mouseState = 0;
+				}
+
+				if (mouseState == 1) {
+				camera.position.x -= (mouseX - camera.position.x) * 0.00005;
+				};
+
+				camera.lookAt( cameraTarget );
+				renderer.render( scene, camera );
+			}
